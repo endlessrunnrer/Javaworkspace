4be0013acc0fc97f4fafeb9dfd3cf5cf5ea1e2eb
Liam_hess <jps312312@gmail.com>
2025-03-04 22:42:22 +0900

    Added chapter 05 and 06

---
+++ MyselfLearningJava/chapter04/src/ForPrintFrom1to1000.java
@@ -0,0 +1,8 @@
+public class ForPrintFrom1to1000 {
+    public static void main(String[] args) {
+        for (int i = 0; i <=10000000; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
